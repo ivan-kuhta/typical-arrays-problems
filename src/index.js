ea3fc7bace0af7ac596bab626dd75be94e7afae7
ivan-kuhta <filmplex22342@gmail.com>
2021-02-21 02:52:56 +0200

    implement 3 functions: max, min, and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+function checkArray(array) {
+  return array instanceof Array && array.length != 0;
+}
 
 exports.min = function min (array) {
-  return 0;
+if(checkArray(array)){
+  return array.sort((a,b) => a - b)[0];
+}
+return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+if(checkArray(array)){
+  return array.sort((a,b) => b - a)[0];
+}
+return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+if(checkArray(array)){
+  return array.reduce((sum, curr) => sum + curr)/array.length;
+}
+return 0;
 }
